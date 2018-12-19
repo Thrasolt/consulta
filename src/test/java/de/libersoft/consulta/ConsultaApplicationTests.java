050d72f0fbc53630bcc189f0fe5f58e5a3656197
timtressel <timres@gmx.net>
2018-12-19 16:18:06 +0100

    setup: initial commit

---
+++ src/test/java/de/libersoft/consulta/ConsultaApplicationTests.java
@@ -0,0 +1,17 @@
+package de.libersoft.consulta;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConsultaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
